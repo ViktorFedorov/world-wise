ecc4f15660996bdae9a23c5d16874738903c97ed
ViktorF <index@user.mail>
2023-11-30 08:08:08 +0300

    leaflet start

--- src/copmponents/map/map.jsx
+++ src/copmponents/map/map.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import styles from './map.module.css'
+import { useSearchParams } from 'react-router-dom'
+import { useCities } from '../../context/cities-context.jsx'
 
 const Map = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+  const { cities } = useCities()
 
-  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [mapPosition, setMapPosition] = useState([
+    52.53586782505711, 13.376933665713324
+  ])
+
+  // const lat = searchParams.get('lat')
+  // const lng = searchParams.get('lng')
+  //
+  // useEffect(() => {
+  //   if (!lat && !lng) return
+  //   setMapPosition([+lat, +lng])
+  // }, [lat, lng])
+  //
+  // console.log(mapPosition)
 
   return (
-    <div className={styles.map} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat} {lng}
-      </h2>
-    </div>
+    // <div className={styles.mapContainer}>
+    <MapContainer
+      center={mapPosition}
+      zoom={13}
+      scrollWheelZoom={true}
+      className={styles.map}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      {cities.map((city) => (
+        <Marker
+          position={[city.position.lat, city.position.lng]}
+          key={city.id}
+        />
+      ))}
+      {/*<Marker position={mapPosition}>*/}
+      {/*  <Popup>*/}
+      {/*    A pretty CSS3 popup. <br /> Easily customizable.*/}
+      {/*  </Popup>*/}
+      {/*</Marker>*/}
+    </MapContainer>
+    // </div>
   )
 }
 
