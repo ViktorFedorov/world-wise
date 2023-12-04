575b556ffecfb267c85721784a5e23d8fbea0b8e
ViktorF <index@user.mail>
2023-12-04 08:01:59 +0300

    start refactoring city-context component for useReduser

--- src/context/cities-context.jsx
+++ src/context/cities-context.jsx
@@ -1,6 +1,10 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import tr from 'react-datepicker'
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState
+} from 'react'
 
 const BASE_URL = 'http://localhost:3000'
 const CitiesContext = createContext(null)
@@ -16,85 +20,124 @@ const uniqCountries = (cities) => {
   return countries
 }
 
+const initialState = {
+  cities: [],
+  currentCity: {},
+  isLoading: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_CITIES':
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...action.payload]
+      }
+    case 'ADD_CITY':
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload
+      }
+    case 'DELETE_CITY':
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...action.payload]
+      }
+    case 'START_LOADING':
+      return {
+        ...state,
+        isLoading: true
+      }
+    // case 'END_LOADING':
+    //   return {
+    //     ...state,
+    //     isLoading: false
+    //   }
+    default:
+      return state
+  }
+}
+
 const CitiesProvider = ({ children }) => {
-  const [cities, setCities] = useState([])
-  const [currentCity, setCurrentCity] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     const getCities = async () => {
       try {
-        setIsLoading(true)
+        dispatch({ action: 'START_LOADING' })
         const response = await fetch(`${BASE_URL}/cities`)
         const cities = await response.json()
-        setCities(cities)
+        dispatch({ action: 'SET_CITIES', payload: cities })
       } catch {
         alert('loading fail')
-      } finally {
-        setIsLoading(false)
       }
     }
 
     getCities()
   }, [])
 
-  const getCity = async (id) => {
-    setIsLoading(true)
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`)
-      const city = await res.json()
-      setCurrentCity(city)
-    } catch {
-      alert('City not loaded =(')
-    } finally {
-      setIsLoading(false)
-    }
-  }
+  // const getCity = async (id) => {
+  //   setIsLoading(true)
+  //   try {
+  //     const res = await fetch(`${BASE_URL}/cities/${id}`)
+  //     const city = await res.json()
+  //     setCurrentCity(city)
+  //   } catch {
+  //     alert('City not loaded =(')
+  //   } finally {
+  //     setIsLoading(false)
+  //   }
+  // }
 
-  const addCity = async (city) => {
-    try {
-      setIsLoading(true)
-      const res = await fetch(`${BASE_URL}/cities`, {
-        method: 'POST',
-        body: JSON.stringify(city),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      })
-      if (res.ok) {
-        const createdCity = await res.json()
-        setCities([...cities, createdCity])
-      } else {
-        throw new Error('City not added =(')
-      }
-    } catch (err) {
-      alert(err)
-    } finally {
-      setIsLoading(false)
-    }
-  }
+  // const addCity = async (city) => {
+  //   try {
+  //     setIsLoading(true)
+  //     const res = await fetch(`${BASE_URL}/cities`, {
+  //       method: 'POST',
+  //       body: JSON.stringify(city),
+  //       headers: {
+  //         'Content-type': 'application/json; charset=UTF-8'
+  //       }
+  //     })
+  //     if (res.ok) {
+  //       const createdCity = await res.json()
+  //       setCities([...cities, createdCity])
+  //     } else {
+  //       throw new Error('City not added =(')
+  //     }
+  //   } catch (err) {
+  //     alert(err)
+  //   } finally {
+  //     setIsLoading(false)
+  //   }
+  // }
 
-  const removeCity = async (id) => {
-    try {
-      setIsLoading(true)
-      const res = await fetch(`${BASE_URL}/cities/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      })
-      if (res.ok) {
-        const updatedCities = cities.filter((city) => city.id !== id)
-        setCities(updatedCities)
-      } else {
-        throw new Error('City not deleted =(')
-      }
-    } catch (err) {
-      alert(err)
-    } finally {
-      setIsLoading(false)
-    }
-  }
+  // const removeCity = async (id) => {
+  //   try {
+  //     setIsLoading(true)
+  //     const res = await fetch(`${BASE_URL}/cities/${id}`, {
+  //       method: 'DELETE',
+  //       headers: {
+  //         'Content-type': 'application/json; charset=UTF-8'
+  //       }
+  //     })
+  //     if (res.ok) {
+  //       const updatedCities = cities.filter((city) => city.id !== id)
+  //       setCities(updatedCities)
+  //     } else {
+  //       throw new Error('City not deleted =(')
+  //     }
+  //   } catch (err) {
+  //     alert(err)
+  //   } finally {
+  //     setIsLoading(false)
+  //   }
+  // }
+
+  const { cities, isLoading, currentCity, getCity, addCity, removeCity } = state
 
   return (
     <CitiesContext.Provider
