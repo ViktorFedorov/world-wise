b420dcd3643e030176b0665335b34359415938ff
ViktorF <index@user.mail>
2023-12-05 07:14:15 +0300

    refac context

--- src/context/cities-context.jsx
+++ src/context/cities-context.jsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState
-} from 'react'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 
 const BASE_URL = 'http://localhost:3000'
 const CitiesContext = createContext(null)
@@ -34,23 +28,33 @@ const reducer = (state, action) => {
         isLoading: false,
         cities: [...action.payload]
       }
+    case 'OPEN_CITY_DETAILS':
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload
+      }
     case 'ADD_CITY':
       return {
         ...state,
         isLoading: false,
-        cities: action.payload
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload
       }
     case 'DELETE_CITY':
       return {
         ...state,
         isLoading: false,
-        cities: [...action.payload]
+        cities: [...state.cities.filter((city) => city.id !== action.payload)],
+        currentCity: {}
       }
     case 'START_LOADING':
       return {
         ...state,
         isLoading: true
       }
+    case 'ERROR':
+      return alert(action.payload)
     default:
       return state
   }
@@ -62,77 +66,70 @@ const CitiesProvider = ({ children }) => {
   useEffect(() => {
     const getCities = async () => {
       try {
-        dispatch({ action: 'START_LOADING' })
+        dispatch({ type: 'START_LOADING' })
         const response = await fetch(`${BASE_URL}/cities`)
         const cities = await response.json()
-        dispatch({ action: 'SET_CITIES', payload: cities })
+        dispatch({ type: 'SET_CITIES', payload: cities })
       } catch {
-        alert('loading fail')
+        dispatch({ type: 'ERROR', payload: 'loading fail' })
       }
     }
 
     getCities()
   }, [])
 
-  // const getCity = async (id) => {
-  //   setIsLoading(true)
-  //   try {
-  //     const res = await fetch(`${BASE_URL}/cities/${id}`)
-  //     const city = await res.json()
-  //     setCurrentCity(city)
-  //   } catch {
-  //     alert('City not loaded =(')
-  //   } finally {
-  //     setIsLoading(false)
-  //   }
-  // }
+  const getCity = async (id) => {
+    try {
+      dispatch({ type: 'START_LOADING' })
+      const res = await fetch(`${BASE_URL}/cities/${id}`)
+      const city = await res.json()
+      dispatch({ type: 'OPEN_CITY_DETAILS', payload: city })
+    } catch {
+      dispatch({ type: 'ERROR', payload: 'City not loaded =(' })
+    }
+  }
 
-  // const addCity = async (city) => {
-  //   try {
-  //     setIsLoading(true)
-  //     const res = await fetch(`${BASE_URL}/cities`, {
-  //       method: 'POST',
-  //       body: JSON.stringify(city),
-  //       headers: {
-  //         'Content-type': 'application/json; charset=UTF-8'
-  //       }
-  //     })
-  //     if (res.ok) {
-  //       const createdCity = await res.json()
-  //       setCities([...cities, createdCity])
-  //     } else {
-  //       throw new Error('City not added =(')
-  //     }
-  //   } catch (err) {
-  //     alert(err)
-  //   } finally {
-  //     setIsLoading(false)
-  //   }
-  // }
+  const addCity = async (city) => {
+    try {
+      dispatch({ type: 'START_LOADING' })
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: 'POST',
+        body: JSON.stringify(city),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      })
+      if (res.ok) {
+        const createdCity = await res.json()
+        dispatch({ type: 'ADD_CITY', payload: createdCity })
+      } else {
+        throw new Error('City not added =(')
+      }
+    } catch (err) {
+      dispatch({ type: 'ERROR', payload: err })
+    }
+  }
 
-  // const removeCity = async (id) => {
-  //   try {
-  //     setIsLoading(true)
-  //     const res = await fetch(`${BASE_URL}/cities/${id}`, {
-  //       method: 'DELETE',
-  //       headers: {
-  //         'Content-type': 'application/json; charset=UTF-8'
-  //       }
-  //     })
-  //     if (res.ok) {
-  //       const updatedCities = cities.filter((city) => city.id !== id)
-  //       setCities(updatedCities)
-  //     } else {
-  //       throw new Error('City not deleted =(')
-  //     }
-  //   } catch (err) {
-  //     alert(err)
-  //   } finally {
-  //     setIsLoading(false)
-  //   }
-  // }
+  const removeCity = async (id) => {
+    try {
+      dispatch({ type: 'START_LOADING' })
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      })
+      if (res.ok) {
+        dispatch({ type: 'DELETE_CITY', payload: id })
+      } else {
+        throw new Error('City not deleted =(')
+      }
+    } catch (err) {
+      dispatch({ type: 'ERROR', payload: err })
+    }
+  }
 
-  const { cities, isLoading, currentCity, getCity, addCity, removeCity } = state
+  const { cities, currentCity, isLoading } = state
 
   return (
     <CitiesContext.Provider
